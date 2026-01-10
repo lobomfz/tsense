ea3560b666fc1bc266a029d2112d9f1d28476d9d
lobomfz <lobomfz@protonmail.com>
2026-01-10 17:07:38 -0300

    fmt

--- src/transformers/date.ts
+++ src/transformers/date.ts
@@ -1,8 +1,8 @@
 import type { FieldTransformer } from "./types.js";
 
 export const DateTransformer: FieldTransformer<Date, number> = {
-  match: (expr, domain) => expr === "Date" || domain === "Date",
-  storageType: "int64",
-  serialize: (date) => date.getTime(),
-  deserialize: (ts) => new Date(ts),
+	match: (expr, domain) => expr === "Date" || domain === "Date",
+	storageType: "int64",
+	serialize: (date) => date.getTime(),
+	deserialize: (ts) => new Date(ts),
 };
